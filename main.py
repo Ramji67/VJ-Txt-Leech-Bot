7d6a9f09d1d62290c1c3055b9079f779e134c3c4
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-02-15 21:41:03 +0530

    main.py

--- main.py
+++ main.py
@@ -194,7 +194,7 @@ async def upload(bot: Client, m: Message):
              id =  url.split("/")[-2]
              url = f"https://stream.pwjarvis.app/{id}/hls/{raw_text2}/main.m3u8"
 
-           copied_name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+           name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
             name = f'{str(count).zfill(3)}) {name1[:60]}'
 
             if "youtu" in url:
