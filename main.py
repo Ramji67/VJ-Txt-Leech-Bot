533af6fb5ba519e2f9344b60f118dcaa282441fc
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-02-15 22:14:40 +0530

    main.py

--- main.py
+++ main.py
@@ -215,11 +215,12 @@ async def upload(bot: Client, m: Message):
       f'**📚 Course :** {raw_text0}\\n\\n'
       f'**🌟 Extracted By :  {MR}**')
 
-                cc1 = f'**——— ✦**  {str(count).zfill(3)}**——— ✦ **\n\n"
-      f'**📁 Title** {copied_name1}\n"
-      f'       **├── Extention : @Course_diploma_bot**\n\n"
-      f' **📚 Course :** {raw_text0}\n\n"
-      f'**🌟 Extracted By {MR}**"             
+              cc1 = (f'**——— ✦**  {str(count).zfill(3)}**——— ✦ **\\n\\n'
+       f'**📁 Title** {copied_name1}\\n'
+       f'       **├── Extention : @Course_diploma_bot**\\n\\n'
+       f' **📚 Course :** {raw_text0}\\n\\n'
+       f'**🌟 Extracted By {MR}**')
+           
                 if "*" in url:
                      a, k = url.split("*", 1)
                      url = a
