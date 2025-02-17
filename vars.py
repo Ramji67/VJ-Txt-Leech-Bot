7e7cfda1907676262c714c7e69a41819f9d054a4
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-02-17 20:28:26 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,9 +5,9 @@
 from os import environ
 
 
-BOT_NAME = "Luminatbot"
+BOT_NAME = "demo"
 API_ID    = 28088290
 API_HASH  = "6998f2c585fdce65ac72dfa23d02b6ec"
-BOT_TOKEN = ""
+BOT_TOKEN = "7676127286:AAGA99obWbphTO9DNO3oNtWyIzM4BQ-e9rw"
 #MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
 MONGO_URI = "mongodb+srv://tusartxt7570:K1AVnnJlDceZBiRY>@cluster0.78fhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
