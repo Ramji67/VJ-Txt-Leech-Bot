f62eb6f95c31f6bd5d00e3eeb84ba769deeebdaa
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-02-17 19:03:34 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,10 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28088290"))
-API_HASH = environ.get("API_HASH", "6998f2c585fdce65ac72dfa23d02b6ec")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
+BOT_NAME = "Luminatbot"
+API_ID    = 28088290
+API_HASH  = "6998f2c585fdce65ac72dfa23d02b6ec"
+BOT_TOKEN = ""
+#MONGO_URI = "mongodb+srv://psrathore1155:<db_password>@cluster0.rpaat.mongodb.net/"
+MONGO_URI = "mongodb+srv://tusartxt7570:K1AVnnJlDceZBiRY>@cluster0.78fhl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
